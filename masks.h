ae309582729387ee17aceaaba6ffceb2f41a86af
NoahSergent <noahsergent78@gmail.com>
2025-12-10 18:00:07 -0500

    copied in spinner code, this version compiles

--- masks.h
+++ masks.h
@@ -13,13 +13,13 @@
 
 
 // SPI Expansion Outputs
-extern const uint8_t TOP_LANE0_LED[] = { 0, 0 };
-extern const uint8_t TOP_LANE1_LED[] = { 0, 0 };
+extern const uint8_t TOP_LANE0_LED[2];
+extern const uint8_t TOP_LANE1_LED[2];
 
 
 // SPI Expansion Inputs
-extern const uint8_t TOP_LANE0_SWITCH[] = { 0, 0 };
-extern const uint8_t TOP_LANE1_SWITCH[] = { 0, 0 };
+extern const uint8_t TOP_LANE0_SWITCH[2];
+extern const uint8_t TOP_LANE1_SWITCH[2];
 
 
 #endif
--- top_lanes.h
+++ top_lanes.h
@@ -1,47 +1,47 @@
-/*
- * top_lanes.h
- *
- * 
- */
+// /*
+//  * top_lanes.h
+//  *
+//  * 
+//  */
 
-#ifndef TOP_LANES_H
-#define TOP_LANES_H
+// #ifndef TOP_LANES_H
+// #define TOP_LANES_H
 
-const uint8_t TOP_LANES_BONUS = 50;
+// const uint8_t TOP_LANES_BONUS = 50;
 
-class TopLanes {
-public:
-    enum LED { LED0,
-               LED1 };
+// class TopLanes {
+// public:
+//     enum LED { LED0,
+//                LED1 };
 
-    enum Switches { SWITCH0,
-                    SWITCH1 };
+//     enum Switches { SWITCH0,
+//                     SWITCH1 };
 
-    static void init() {
-    }
+//     static void init() {
+//     }
 
-    static void switchTriggered( Switches triggered ) {
-        switch ( triggered ) {
-        case SWITCH0:
+//     static void switchTriggered( Switches triggered ) {
+//         switch ( triggered ) {
+//         case SWITCH0:
             
-            break;
-        case SWITCH1:
-            break;
-        }
-    }
+//             break;
+//         case SWITCH1:
+//             break;
+//         }
+//     }
 
-private:
-    static void onLED( LED ) {
-    }
+// private:
+//     static void onLED( LED ) {
+//     }
 
-    static void offLED( LED ) {
-    }
+//     static void offLED( LED ) {
+//     }
 
-    static void awardBonus() {
-        Scoreboard::addToScore( TOP_LANES_BONUS );
-    }
+//     static void awardBonus() {
+//         Scoreboard::addToScore( TOP_LANES_BONUS );
+//     }
 
-    static bool LEDs[2];
-};
+//     static bool LEDs[2];
+// };
 
-#endif
+// #endif
